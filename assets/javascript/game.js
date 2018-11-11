84ff74ae01625d3cab250f53e100fd6cf205c040
Winfield H <winterfreshness@gmail.com>
2018-11-11 16:48:31 -0500

    Got the fight func working.  Must rework pkmn choice, add checkFeint/win

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,15 +8,16 @@ function Pokemon(name, hp, weak, strong) {
     this.hp = hp;
     this.status = "OK";
     this.moves = [{
-        atkName: weak[0],
-        str: weak[1],
-        acc: weak[2]
-    },
-    {
-        atkName: strong[0],
-        str: strong[1],
-        acc: strong[2]
-    }];
+            atkName: weak[0],
+            str: weak[1],
+            acc: weak[2]
+        },
+        {
+            atkName: strong[0],
+            str: strong[1],
+            acc: strong[2]
+        }
+    ];
 }
 
 // Fighting logic
@@ -33,12 +34,35 @@ function fight(myPokemon, rivalPokemon) {
     }
 
     myMove = prompt(`Choose your move:\n0) ${myPokemon.moves[0].atkName} \n1) ${myPokemon.moves[1].atkName}`);
-    
+
     console.log(`My move: ${myPokemon.moves[myMove].atkName}`);
     console.log(`Rival move: ${rivalPokemon.moves[rivalMove].atkName}`);
 
+    // check for hit and deal damage
+    let dmg1 = accuracy(myPokemon.name, myPokemon.moves[myMove]);
+    let dmg2 = accuracy(rivalPokemon.name, rivalPokemon.moves[rivalMove]);
+    rivalPokemon.hp -= dmg1;
+    myPokemon.hp -= dmg2;
+
+    console.log(`My Pokemon's HP: ${myPokemon.hp}`);
+    console.log(`Rival Pokemon's HP: ${rivalPokemon.hp}`);
 }
 
+function accuracy(pkmn, move) {
+    let randNum = (Math.random() * 10);
+    
+    if (randNum <= move.acc) {
+        alert(`${pkmn} hit for ${move.str} damage!`);
+        return move.str;
+    } else {
+        alert(`${pkmn} missed!`);
+        return 0;
+    }
+}
+
+// ========== MAIN CODE STARTS HERE ==========
+
+// Initialize
 // make the pokemon for my team
 myTeam.push(new Pokemon("Blastoise", 100, ["Weak Atk", 10, 9], ["Strong Atk", 15, 6.5]));
 myTeam.push(new Pokemon("Charizard", 100, ["Weak Atk", 10, 9], ["Strong Atk", 15, 6.5]));
@@ -55,8 +79,10 @@ rivalTeam.push(new Pokemon("Lugia", 100, ["Weak Atk", 10, 9], ["Strong Atk", 15,
 let rivalChoice = Math.floor(Math.random() * 3);
 let choice = prompt(`Choose your Pokemon:\n0) ${myTeam[0].name}\n1) ${myTeam[1].name}\n2) ${myTeam[2].name}`);
 
-// alert(`Player chooses: ${myTeam[choice].name}`);
-// alert(`Rival chooses: ${rivalTeam[rivalChoice].name}`);
+console.log(`Player chooses: ${myTeam[choice].name}`);
+console.log(`Rival chooses: ${rivalTeam[rivalChoice].name}`);
 
 // FIGHTING (loop until there is a feint)
 fight(myTeam[choice], rivalTeam[rivalChoice]);
+// checkFeint(myTeam[choice], rivalTeam[rivalChoice]);
+// checkWin();
