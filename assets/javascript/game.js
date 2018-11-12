86ec3cc9d3ac090e30039619614be8745893d91b
Winfield H <winterfreshness@gmail.com>
2018-11-11 22:30:20 -0500

    Finished the logic of the game. Needs style and j.query

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -36,8 +36,8 @@ function fight(myPokemon, rivalPokemon) {
 
     myMove = prompt(`Choose your move:\n0) ${myPokemon.moves[0].atkName} \n1) ${myPokemon.moves[1].atkName}`);
 
-    console.log(`My move: ${myPokemon.moves[myMove].atkName}`);
-    console.log(`Rival move: ${rivalPokemon.moves[rivalMove].atkName}`);
+    // console.log(`My move: ${myPokemon.moves[myMove].atkName}`);
+    // console.log(`Rival move: ${rivalPokemon.moves[rivalMove].atkName}`);
 
     // check for hit and deal damage
     let dmg1 = accuracy(myPokemon.name, myPokemon.moves[myMove]);
@@ -45,13 +45,16 @@ function fight(myPokemon, rivalPokemon) {
     rivalPokemon.hp -= dmg1;
     myPokemon.hp -= dmg2;
 
+    // the player's Pokemon gets stronger
+    myPokemon.moves[myMove].str += 2;
+
     console.log(`My Pokemon's HP: ${myPokemon.hp}`);
     console.log(`Rival Pokemon's HP: ${rivalPokemon.hp}`);
 }
 
 function accuracy(pkmn, move) {
     let randNum = (Math.random() * 10);
-    
+
     if (randNum <= move.acc) {
         alert(`${pkmn} hit for ${move.str} damage!`);
         return move.str;
@@ -61,10 +64,10 @@ function accuracy(pkmn, move) {
     }
 }
 
-function checkFeint(pkmn) {
+function checkFaint(pkmn) {
     if (pkmn.hp <= 0) {
-        pkmn.status = "feint";
-        alert(`${pkmn.name} has feinted.`);
+        pkmn.status = "faint";
+        alert(`${pkmn.name} has fainted.`);
         return true;
     }
 }
@@ -73,15 +76,15 @@ function checkFeint(pkmn) {
 
 // Initialize
 // make the pokemon for my team
-myTeam.push(new Pokemon("Blastoise", 100, ["Weak Atk", 10, 9], ["Strong Atk", 15, 6.5]));
-myTeam.push(new Pokemon("Charizard", 100, ["Weak Atk", 10, 9], ["Strong Atk", 15, 6.5]));
-myTeam.push(new Pokemon("Venusaur", 100, ["Weak Atk", 10, 9], ["Strong Atk", 15, 6.5]));
+myTeam.push(new Pokemon("Blastoise", 79, ["Weak Atk", 8, 9], ["Strong Atk", 13, 6.5]));
+myTeam.push(new Pokemon("Charizard", 78, ["Weak Atk", 8, 9], ["Strong Atk", 13, 6.5]));
+myTeam.push(new Pokemon("Venusaur", 80, ["Weak Atk", 8, 9], ["Strong Atk", 13, 6.5]));
 // console.log(myTeam);
 
 // make the pokemon for the rival team
-rivalTeam.push(new Pokemon("Raichu", 00, ["Weak Atk", 10, 9], ["Strong Atk", 15, 6.5]));
-rivalTeam.push(new Pokemon("Umbreon", 00, ["Weak Atk", 10, 9], ["Strong Atk", 15, 6.5]));
-rivalTeam.push(new Pokemon("Lugia", 00, ["Weak Atk", 10, 9], ["Strong Atk", 15, 6.5]));
+rivalTeam.push(new Pokemon("Raichu", 60, ["Weak Atk", 9, 9], ["Strong Atk", 14, 6.5]));
+rivalTeam.push(new Pokemon("Espeon", 65, ["Weak Atk", 6, 9], ["Strong Atk", 11, 6.5]));
+rivalTeam.push(new Pokemon("Lugia", 106, ["Weak Atk", 9, 9], ["Strong Atk", 14, 6.5]));
 // console.log(rivalTeam);
 
 // choose the Pokemon you want to use and computer chooses it's Pokemon
@@ -91,23 +94,37 @@ let choice = prompt(`Choose your Pokemon:\n0) ${myTeam[0].name}\n1) ${myTeam[1].
 console.log(`Player chooses: ${myTeam[choice].name}`);
 console.log(`Rival chooses: ${rivalTeam[rivalChoice].name}`);
 
-// FIGHTING (loop until there is a feint)
-fight(myTeam[choice], rivalTeam[rivalChoice]);
-if (checkFeint(myTeam[choice])) {
-    // hide the feinted pokemon's element
-    choice = prompt(`Choose your Pokemon:\n0) ${myTeam[0].name}\n1) ${myTeam[1].name}\n2) ${myTeam[2].name}`);
-    alert(`Player sent out ${myTeam[choice].name}.`);
-}
-if (checkFeint(rivalTeam[rivalChoice])) {
-    let finished = false;
-    while (finished === false) {
-        let randNum = Math.floor(Math.random() * 3);
-        if (rivalTeam[randNum].status === "OK") {
-            rivalChoice = randNum;
-            alert(`Rival sent out ${rivalTeam[rivalChoice].name}.`);
-            finished = true;
+// FIGHTING (loop until there is a faint)
+do {
+    fight(myTeam[choice], rivalTeam[rivalChoice]);
+    if (checkFaint(myTeam[choice])) {
+        // hide the fainted pokemon's element
+        if (myTeam.every(obj => obj.status == "faint")) {
+            // end game and reset
+            game = false;
+            alert(`Your Pokemon team loses...`);
+        } else {
+            choice = prompt(`Choose your Pokemon:\n0) ${myTeam[0].name}\n1) ${myTeam[1].name}\n2) ${myTeam[2].name}`);
+            alert(`Player sent out ${myTeam[choice].name}.`);
         }
     }
-}
-// checkWin();
-// game = false ==> reset game
+    if (checkFaint(rivalTeam[rivalChoice])) {
+        if (rivalTeam.every(obj => obj.status == "faint")) {
+            // end game and reset
+            game = false;
+            alert(`Your Pokemon team wins!`);
+
+        } else {
+            let finished = false;
+            while (finished === false) {
+                let randNum = Math.floor(Math.random() * 3);
+                if (rivalTeam[randNum].status === "OK") {
+                    rivalChoice = randNum;
+                    alert(`Rival sent out ${rivalTeam[rivalChoice].name}.`);
+                    finished = true;
+                }
+            }
+        }
+
+    }
+} while (game === true);
