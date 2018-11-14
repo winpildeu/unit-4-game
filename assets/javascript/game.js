55da69a0e9d24808a8d6339a6bcf542c1ed15760
Winfield H <winterfreshness@gmail.com>
2018-11-13 21:38:05 -0500

    Got fighting to work. Haven't changed pokemon yet.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,7 @@ let myTeam = [];
 let rivalTeam = [];
 let game = true;
 let choice;
+let rivalChoice;
 
 // make a Pokemon object w/ a constructor function
 function Pokemon(name, hp, weak, strong) {
@@ -23,19 +24,9 @@ function Pokemon(name, hp, weak, strong) {
 }
 
 // fighting logic
-function fight(myPokemon, rivalPokemon) {
-    let myMove;
-    let rivalMove;
-
+function fight(myPokemon, rivalPokemon, myMove) {
     // choose a move and rival chooses a random move
-    let randNum = Math.random() * 2;
-    if (randNum <= 1) {
-        rivalMove = 0;
-    } else {
-        rivalMove = 1;
-    }
-
-    myMove = prompt(`Choose your move:\n0) ${myPokemon.moves[0].atkName} \n1) ${myPokemon.moves[1].atkName}`);
+    let rivalMove = Math.floor(Math.random() * 2);
 
     // console.log(`My move: ${myPokemon.moves[myMove].atkName}`);
     // console.log(`Rival move: ${rivalPokemon.moves[rivalMove].atkName}`);
@@ -49,8 +40,8 @@ function fight(myPokemon, rivalPokemon) {
     // the player's Pokemon gets stronger after every move attempt
     myPokemon.moves[myMove].str += 2;
 
-    console.log(`My Pokemon's HP: ${myPokemon.hp}`);
-    console.log(`Rival Pokemon's HP: ${rivalPokemon.hp}`);
+    $("#myHp").text(`${myPokemon.hp}`);
+    $("#rivalHp").text(`${rivalPokemon.hp}`);
 }
 
 // check if the move actually hits the other pokemon
@@ -58,25 +49,28 @@ function accuracy(pkmn, move) {
     let randNum = (Math.random() * 10);
 
     if (randNum <= move.acc) {
-        alert(`${pkmn} hit for ${move.str} damage!`);
+        // alert(`${pkmn} hit for ${move.str} damage!`);
         return move.str;
     } else {
-        alert(`${pkmn} missed!`);
+        // alert(`${pkmn} missed!`);
         return 0;
     }
 }
 
 // checks the pokemon's HP to see if it has fainted
-function checkFaint(pkmn) {
-    if (pkmn.hp <= 0) {
-        pkmn.status = "faint";
-        alert(`${pkmn.name} has fainted.`);
-        return true;
+function checkFaint(myPokemon, rivalPokemon) {
+    if(myPokemon.hp <= 0) {
+        alert("You lose...");
+        // reset the game
+    }
+    if (rivalPokemon.hp <= 0) {
+        rivalPokemon.status = "faint";
+        alert("Rival pokemon fainted.");
     }
 }
 
 function chooseRivalPokemon() {
-    let rivalChoice = Math.floor(Math.random() * 3);
+    rivalChoice = Math.floor(Math.random() * 3);
     if (rivalChoice == 0) {
         $("#rai").css("display", "inline");
     } else if (rivalChoice == 1) {
@@ -85,8 +79,19 @@ function chooseRivalPokemon() {
         $("#lug").css("display", "inline");
     }
     $("#rivalHp").html(`<p>${rivalTeam[rivalChoice].hp}</p>`);
+}
 
-    // $("#info").html(`Rival selected ${rivalTeam[rivalChoice].name}.`);
+function hideSelectScreen(num) {
+    $("#info").html(` You selected ${myTeam[num].name}.`);
+    setTimeout(function () {
+        $("#info").hide();
+    }, 1000);
+    $("#battle").hide(1000);
+    $("#pokemonSelect").hide(1000);
+}
+
+function showFightScreen() {
+
 }
 // ========== MAIN CODE STARTS HERE ==========
 
@@ -95,13 +100,11 @@ function chooseRivalPokemon() {
 myTeam.push(new Pokemon("Venusaur", 80, ["Weak Atk", 8, 9], ["Strong Atk", 13, 6.5]));
 myTeam.push(new Pokemon("Charizard", 78, ["Weak Atk", 8, 9], ["Strong Atk", 13, 6.5]));
 myTeam.push(new Pokemon("Blastoise", 79, ["Weak Atk", 8, 9], ["Strong Atk", 13, 6.5]));
-// console.log(myTeam);
 
 // make the pokemon for the rival team
 rivalTeam.push(new Pokemon("Raichu", 60, ["Weak Atk", 9, 9], ["Strong Atk", 14, 6.5]));
 rivalTeam.push(new Pokemon("Espeon", 65, ["Weak Atk", 6, 9], ["Strong Atk", 11, 6.5]));
 rivalTeam.push(new Pokemon("Lugia", 106, ["Weak Atk", 9, 9], ["Strong Atk", 14, 6.5]));
-// console.log(rivalTeam);
 
 // // FIGHTING (loop until there is a faint)
 // do {
@@ -161,46 +164,45 @@ $(document).ready(function () {
         $(this).css("border-color", "black");
     });
 
-    // selecting pokemon
+    // selecting pokemon and switch to fight screen
     $("#v").click(function () {
         choice = 0;
-        $("#info").html(` You selected ${myTeam[choice].name}.`);
-        setTimeout(function () {
-            $("#info").hide();
-        }, 1000);
-        // $("#battle").hide(1000);
-        $("#pokemonSelect").hide(1000);
+        hideSelectScreen(choice);
+        // Fight screen setup
         $("#ven").css("display", "inline");
         chooseRivalPokemon();
         $("#fightBox").show(1000);
         $("#myHp").html(`<p>${myTeam[choice].hp}</p>`);
+        $("#atkButtons").show(2000);
     });
     $("#c").click(function () {
         choice = 1;
-        $("#info").html(` You selected ${myTeam[choice].name}.`);
-        setTimeout(function () {
-            $("#info").hide();
-        }, 1000);
-        $("#battle").hide(1000);
-        $("#pokemonSelect").hide(1000);
+        hideSelectScreen(choice);
+        // Fight screen setup
         $("#cha").css("display", "inline");
         chooseRivalPokemon();
         $("#fightBox").show(1000);
         $("#myHp").html(`<p>${myTeam[choice].hp}</p>`);
-
+        $("#atkButtons").show(2000);
     });
     $("#b").click(function () {
         choice = 2;
-        $("#info").html(` You selected ${myTeam[choice].name}.`);
-        setTimeout(function () {
-            $("#info").hide();
-        }, 1000);
-        $("#battle").hide(1000);
-        $("#pokemonSelect").hide(1000);
+        hideSelectScreen(choice);
+        // Fight screen setup
         $("#bla").css("display", "inline");
         chooseRivalPokemon();
         $("#fightBox").show(1000);
         $("#myHp").html(`<p>${myTeam[choice].hp}</p>`);
+        $("#atkButtons").show(2000);
     });
 
+    // Attack and fighting events
+    $("#quickAtk").click(function() {
+        fight(myTeam[choice], rivalTeam[rivalChoice], 0);
+        checkFaint(myTeam[choice], rivalTeam[rivalChoice]);
+    });
+    $("#strongAtk").click(function() {
+        fight(myTeam[choice], rivalTeam[rivalChoice], 1);
+        checkFaint(myTeam[choice], rivalTeam[rivalChoice]);
+    });
 });
